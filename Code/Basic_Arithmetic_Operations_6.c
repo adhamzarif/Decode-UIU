9a94d2fe12a3931fc8ed44c1e1b0ee57362b07fc
adhamzarif <zarifseam52@gmail.com>
2025-05-20 14:29:23 +0600

    Backup From ArchLinux

---
+++ Code/Basic_Arithmetic_Operations_6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    long long int m, n;
+    scanf("%lld %lld", &m, &n);
+    printf("%lld\n", m + n);
+    printf("%lld\n", m - n);
+    printf("%lld\n", m * n);
+    printf("%lld\n", m / n);
+    return 0;
+}
